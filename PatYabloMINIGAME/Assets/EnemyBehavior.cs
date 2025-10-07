117f8ef155caba2ae9480f67c7dd9f45a42a183b
PatY-GD <pyablons@student.fitchburgstate.edu>
2025-10-07 14:20:58 -0400

    Touch Up

--- PatYabloMINIGAME/Assets/EnemyBehavior.cs
+++ PatYabloMINIGAME/Assets/EnemyBehavior.cs
@@ -10,7 +10,7 @@ public class EnemyBehavior : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        // Enemy will follow the player
     }
 
     // Update is called once per frame
